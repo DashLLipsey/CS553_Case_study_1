1360bb93e5c8e968b0a8ac1e634963d1e2049a4d
Ryan Wright <rcwright@wpi.edu>
2026-02-04 21:58:02 -0500

    passed token to model to see if it works now

--- app.py
+++ app.py
@@ -162,7 +162,8 @@ def respond(
             )'''
             pipe = pipeline(
                 "text-generation",
-                model="microsoft/Phi-3-mini-4k-instruct"
+                model="microsoft/Phi-3-mini-4k-instruct",
+                token=hf_token
             )
 
         prompt = "\n".join([f"{m['role']}: {m['content']}" for m in messages])
